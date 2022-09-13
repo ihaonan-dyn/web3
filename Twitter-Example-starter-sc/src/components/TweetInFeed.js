0f95e114c95e0c08755e089844595a72ecc9a5b6
songhaonan <1660195036@qq.com>
2022-09-13 14:39:02 +0800

    用户每发帖一次获得一枚NLT代币（合约部分用remix开发部署，代码供参考，无实际用处）

--- Twitter-Example-starter-sc/src/components/TweetInFeed.js
+++ Twitter-Example-starter-sc/src/components/TweetInFeed.js
@@ -1,77 +1,83 @@
-import React,{useEffect,useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./TweetInFeed.css";
 import filecoinOrbit from "../images/filecoinOrbit.jpeg";
 import canoe from "../images/canoe.jpeg";
-import { defaultImgs } from "../defaultimgs";
+import {defaultImgs} from "../defaultimgs";
 import pfp4 from "../images/pfp4.png";
 import pfp5 from "../images/pfp5.png";
-import { Icon } from "web3uikit";
+import {Icon} from "web3uikit";
 import {useMoralis} from "react-moralis";
+
 const TweetInFeed = (profile) => {
-  const {Moralis,account} =useMoralis();
-  const [tweetAtr, setTweetAtr] = useState();
-  useEffect(() => {
-   async function getTweets(){
-    try{
-      const Tweets=Moralis.Object.extend("Tweets");
-      const  query=new Moralis.Query(Tweets)
-        if(profile.profile){
-          query.equalTo("tweetAcc",account);
+    const {Moralis, account, isInitialized} = useMoralis();
+    const [tweetAtr, setTweetAtr] = useState();
+    useEffect(() => {
+        async function getTweets() {
+            try {
+                const Tweets = Moralis.Object.extend("Tweets");
+                const query = new Moralis.Query(Tweets)
+                if (profile.profile) {
+                    query.equalTo("tweetAcc", account);
+                }
+                const results = await query.find();
+                setTweetAtr(results);
+            } catch (error) {
+                console.log(error);
+            }
         }
-        const results=await query.find();
-        setTweetAtr(results);
-    }catch(error){
-      console.log(error);
-    }
-   }
-   getTweets();
-  }, [profile])
-  return (
-    <>
-    {tweetAtr?.map((e)=>{
-      return(
-      <div className="feedTweet">
-        <img src={e.attributes.tweeterPfp?e.attributes.tweeterPfp: defaultImgs[0]} className="profilePic"></img>
-        {/* <img src={user.attributes.pfp?user.attributes.pfp: defaultImgs[0]} className="profilePic"></img> */}
-        <div className="completeTweet">
-          <div className="who">
-            {e.attributes.tweeterUserName.slice(0,10)}
-            <div className="accWhen">
-              {`${e.attributes.tweeterAcc.slice(0,4)}...${e.attributes.tweeterAcc.slice(38)} 
-              ${e.attributes.createdAt.toLocaleString('en-us',{month:'short'})}
-              ${e.attributes.createdAt.toLocaleString('en-us',{day:'numeric'})}
+
+        if (isInitialized) {
+            getTweets();
+        }
+
+    }, [profile])
+    return (
+        <>
+            {tweetAtr?.map((e) => {
+                return (
+                    <div className="feedTweet">
+                        <img src={e.attributes.tweeterPfp ? e.attributes.tweeterPfp : defaultImgs[0]}
+                             className="profilePic"></img>
+                        {/* <img src={user.attributes.pfp?user.attributes.pfp: defaultImgs[0]} className="profilePic"></img> */}
+                        <div className="completeTweet">
+                            <div className="who">
+                                {e.attributes.tweeterUserName.slice(0, 10)}
+                                <div className="accWhen">
+                                    {`${e.attributes.tweeterAcc.slice(0, 4)}...${e.attributes.tweeterAcc.slice(38)} 
+              ${e.attributes.createdAt.toLocaleString('en-us', {month: 'short'})}
+              ${e.attributes.createdAt.toLocaleString('en-us', {day: 'numeric'})}
               `}
-            </div>
-          </div>
-          <div className="tweetContent">
-            {
-              e.attributes.tweetTxt
+                                </div>
+                            </div>
+                            <div className="tweetContent">
+                                {
+                                    e.attributes.tweetTxt
+                                }
+                                {
+                                    e.attributes.tweetImg && (
+                                        <img src={e.attributes.tweetImg} className="tweetImg"></img>
+                                    )
+                                }
+
+                            </div>
+                            <div className="interactions">
+                                <div className="interactionNums">
+                                    <Icon fill="#3f3f3f" size={20} svg="messageCircle"/>
+                                </div>
+                                <div className="interactionNums">
+                                    <Icon fill="#3f3f3f" size={20} svg="star"/>
+                                    12
+                                </div>
+                                <div className="interactionNums">
+                                    <Icon fill="#3f3f3f" size={20} svg="matic"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>);
+            }).reverse()
             }
-            {
-               e.attributes.tweetImg&&(
-                <img src={e.attributes.tweetImg} className="tweetImg"></img>
-               )
-            }
-            
-          </div>
-          <div className="interactions">
-            <div className="interactionNums">
-              <Icon fill="#3f3f3f" size={20} svg="messageCircle" />
-            </div>
-            <div className="interactionNums">
-              <Icon fill="#3f3f3f" size={20} svg="star" />
-              12
-            </div>
-            <div className="interactionNums">
-              <Icon fill="#3f3f3f" size={20} svg="matic" />
-            </div>
-          </div>
-        </div>
-      </div>);
-    }).reverse()
-    }
-    </>
-  );
+        </>
+    );
 };
 
 export default TweetInFeed;
--- Twitter-Example-starter-sc/src/pages/Home.js
+++ Twitter-Example-starter-sc/src/pages/Home.js
@@ -4,14 +4,15 @@ import { useState, useRef } from "react";
 import { defaultImgs } from "../defaultimgs";
 import { TextArea, Icon } from "web3uikit";
 import TweetInFeed from "../components/TweetInFeed";
-import { useMoralis, useWeb3ExecuteFunction } from "react-moralis"
-import Tweets from "../contractABIs/Tweets.json"
+import { useMoralis, useWeb3ExecuteFunction} from "react-moralis";
+import Tweets from "../contractABIs/Tweets.json";
+import Faucet from "../contractABIs/Faucet.json";
 const Home = () => {
   const inputFile = useRef(null);
   const [selectedFile, setSelectedFile] = useState();
   const [theFile, setTheFile] = useState();
   const [tweet, setTweet] = useState();
-  const { isAuthenticated, Moralis } = useMoralis()
+  const { isAuthenticated, Moralis ,account} = useMoralis()
   const contractProcessor = useWeb3ExecuteFunction();
   // const user =Moralis.User.current();
   const onImageClick = () => {
@@ -69,6 +70,7 @@ const Home = () => {
     contractProcessor.fetch({
       params: options,
       onSuccess: () => {
+        postTransferToken();
         saveTweet();
       },
       onError: (error) => {
@@ -76,64 +78,82 @@ const Home = () => {
         console.log(error);
       }
     })
-
+  }
+  //发帖奖励一代币
+  async function postTransferToken() {
+    let options = {
+      contractAddress: "0x58aD35aA4D8F6f9822a4d61A925e81F811579F76",
+      functionName: "requestTokens",
+      abi: Faucet.abi,
+      params: {
+      },
+    }
+    contractProcessor.fetch({
+      params: options,
+      onSuccess: () => {
+      },
+      onError: (error) => {
+        console.log(account);
+        console.log(error);
+      }
+    })
   }
   return (
-    <>
-      <div className="pageIdentify">Home</div>
-      <div className="mainContent">
-        <div className="profileTweet">
-          {isAuthenticated ? (
-            <img
-              src={Moralis.User.current().attributes.pfp ? Moralis.User.current().attributes.pfp : defaultImgs[0]} className="profilePic" >
-            </img>
-          ) : (
-            <img
-            src="	https://ipfs.moralis.io:2053/ipfs/QmNgA9MNWFfRaoKzBt21VghQopnKXBgVxzyGvv5qjsV4Vw/media/1" className="profilePic" >
-          </img>
-          )}
-          <div className="tweetBox">
-            <TextArea
-              label=""
-              placeholder="Type here field"
-              name="tweetTextArea"
-              onBlur={function noRefCheck() { }}
-              type="text"
-              width="95%"
-              onChange={(e) => setTweet(e.target.value)}>
-            </TextArea>
-            {selectedFile && (
-              <img src={selectedFile} className="tweetImg"></img>
+      <>
+        <div className="pageIdentify">Home</div>
+        <div className="mainContent">
+          <div className="profileTweet">
+            {isAuthenticated ? (
+                <img
+                    src={Moralis.User.current().attributes.pfp ? Moralis.User.current().attributes.pfp : defaultImgs[0]} className="profilePic" >
+                </img>
+            ) : (
+                <img
+                    src="	https://ipfs.moralis.io:2053/ipfs/QmNgA9MNWFfRaoKzBt21VghQopnKXBgVxzyGvv5qjsV4Vw/media/1" className="profilePic" >
+                </img>
             )}
+            <div className="tweetBox">
+              <TextArea
+                  label=""
+                  placeholder="Type here field"
+                  name="tweetTextArea"
+                  onBlur={function noRefCheck() { }}
+                  type="text"
+                  width="95%"
+                  onChange={(e) => setTweet(e.target.value)}>
+              </TextArea>
+              {selectedFile && (
+                  <img src={selectedFile} className="tweetImg"></img>
+              )}
 
-            <div className="imgOrTweet">
-              <div className="imgDiv" onClick={onImageClick}>
-                <input
-                  type="file"
-                  name="file"
-                  ref={inputFile}
-                  onChange={changeHandler}
-                  style={{ display: "none" }}
-                />
-                <Icon fill="#1DA1F2" size={20} svg="image"></Icon>
-              </div>
-              {isAuthenticated ? (
-                <div className="tweetOptions">
-                <div className="tweet" onClick={saveTweet}>Tweet</div>
-                <div className="tweet" onClick={maticTweet} style={{ backgroundColor: "#8247e5" }}>
-                  <Icon fill="#ffffff" size={20} svg="matic" />
+              <div className="imgOrTweet">
+                <div className="imgDiv" onClick={onImageClick}>
+                  <input
+                      type="file"
+                      name="file"
+                      ref={inputFile}
+                      onChange={changeHandler}
+                      style={{ display: "none" }}
+                  />
+                  <Icon fill="#1DA1F2" size={20} svg="image"></Icon>
                 </div>
+                {isAuthenticated ? (
+                    <div className="tweetOptions">
+                      <div className="tweet" onClick={saveTweet}>Tweet</div>
+                      <div className="tweet" onClick={maticTweet} style={{ backgroundColor: "#8247e5" }}>
+                        <Icon fill="#ffffff" size={20} svg="matic" />
+                      </div>
+                    </div>
+                ) : (
+                    console.log("111")
+                )}
+
               </div>
-          ) : (
-           console.log("111")
-          )}
-         
             </div>
           </div>
+          <TweetInFeed profile={false} />
         </div>
-        <TweetInFeed profile={false} />
-      </div>
-    </>
+      </>
   );
 };
 
